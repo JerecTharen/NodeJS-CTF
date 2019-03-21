fc95e2716a67762a5eaff94aa1eb6664e85e364f
Jacob Bennett <kf7ags@gmail.com>
2019-03-21 16:32:40 -0600

    in progress

--- CAN-EDIT/get-calls.js
+++ CAN-EDIT/get-calls.js
@@ -1,10 +1,13 @@
-const fs = require('fs');
 
-// const app = require('../NO-EDIT/app');
-//
+// const express = require('express');
+const fs = require('fs');
+// const app = express();
+
 // app.get('/flag', (req, resp)=>{
-//     resp.send(fs.readFile('../INIT-VARIABLES/flag.txt', 'utf8'));
+//    resp.send(fs.readFile('../INIT-VARIABLES/flag.txt', 'utf8'));
 // });
-//
-//
-// module.exports =
+
+module.exports = function(req, resp){
+    console.log('got flag');
+    resp.send(fs.readFile('../INIT-VARIABLES/flag.txt', 'utf8'));
+};
--- NO-EDIT/app.js
+++ NO-EDIT/app.js
@@ -4,8 +4,24 @@ const fs = require('fs');
 const app = express();
 const port = require('../INIT-VARIABLES/port');
 
+
+//component imports
+const teamGetCalls = require('../CAN-EDIT/get-calls');
+
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`);
 });
 
+app.get('/flag', teamGetCalls(req, resp));
+
 module.exports = app;
+
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
+//
+// module.exports = router;
